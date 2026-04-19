f480e69a0243759805ae9bc55d13b8194b8f5b7e
daveofiveo <77298401+daveofiveo@users.noreply.github.com>
2026-04-18 19:48:11 -0500

    Add files via upload

---
+++ email.js
@@ -0,0 +1,10 @@
+(function() {
+    var u = 'david', d = 'dhtech.jp', e = u + '@' + d;
+    var els = [
+        document.getElementById('header-email'),
+        document.getElementById('contact-email')
+    ];
+    els.forEach(function(el) {
+        if (el) { el.href = 'mailto:' + e; el.textContent = e; }
+    });
+})();
